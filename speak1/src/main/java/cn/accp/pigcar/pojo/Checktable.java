3a2970f79c0c38f90286d9e811e86137d1efd3b3
Huanghui <1084158364@qq.com>
2019-03-04 13:59:17 +0800

    pig1

---
+++ speak1/src/main/java/cn/accp/pigcar/pojo/Checktable.java
@@ -0,0 +1,147 @@
+package cn.accp.pigcar.pojo;
+
+import java.util.Date;
+
+/**
+ * Checktable entity.
+ * 
+ * @author MyEclipse Persistence Tools
+ */
+
+public class Checktable implements java.io.Serializable {
+
+
+
+	private Long checkid;
+	private Renttable renttable;
+	private Users users;
+	private Date checkdate;
+	private String field;
+	private String problem;
+	private Double paying;
+	private Long tableid;
+	private String checkUserId;
+
+
+	public String getCheckUserId() {
+		return checkUserId;
+	}
+
+	public void setCheckUserId(String checkUserId) {
+		this.checkUserId = checkUserId;
+	}
+
+	public Long getTableid() {
+		return tableid;
+	}
+
+	public void setTableid(Long tableid) {
+		this.tableid = tableid;
+	}
+
+
+	public Checktable() {
+	}
+
+	public Checktable(Long checkid, Renttable renttable, Users users,
+			Date checkdate) {
+		this.checkid = checkid;
+		this.renttable = renttable;
+		this.users = users;
+		this.checkdate = checkdate;
+	}
+
+	public Checktable(Long checkid, Renttable renttable, Users users,
+			Date checkdate, String field, String problem, Double paying) {
+		this.checkid = checkid;
+		this.renttable = renttable;
+		this.users = users;
+		this.checkdate = checkdate;
+		this.field = field;
+		this.problem = problem;
+		this.paying = paying;
+	}
+
+
+	public Long getCheckid() {
+		return this.checkid;
+	}
+
+	public void setCheckid(Long checkid) {
+		this.checkid = checkid;
+	}
+
+	public Renttable getRenttable() {
+		return this.renttable;
+	}
+
+	public void setRenttable(Renttable renttable) {
+		this.renttable = renttable;
+	}
+
+	public Users getUsers() {
+		return this.users;
+	}
+
+	public void setUsers(Users users) {
+		this.users = users;
+	}
+
+	public Date getCheckdate() {
+		return this.checkdate;
+	}
+
+	public void setCheckdate(Date checkdate) {
+		this.checkdate = checkdate;
+	}
+
+	public String getField() {
+		return this.field;
+	}
+
+	public void setField(String field) {
+		this.field = field;
+	}
+
+	public String getProblem() {
+		return this.problem;
+	}
+
+	public void setProblem(String problem) {
+		this.problem = problem;
+	}
+
+	public Double getPaying() {
+		return this.paying;
+	}
+
+	public void setPaying(Double paying) {
+		this.paying = paying;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((checkid == null) ? 0 : checkid.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		final Checktable other = (Checktable) obj;
+		if (checkid == null) {
+			if (other.checkid != null)
+				return false;
+		} else if (!checkid.equals(other.checkid))
+			return false;
+		return true;
+	}
+
+}
---
+++ speak1/src/main/java/cn/accp/pigcar/pojo/Menus.java
@@ -0,0 +1,155 @@
+package cn.accp.pigcar.pojo;
+
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Menus implements java.io.Serializable {
+
+
+
+	private Long menuid;
+	@Override
+	public String toString() {
+		return "Menus [menuid=" + menuid + ", menuname=" + menuname
+				+ ", connurl=" + connurl + ", fatherid=" + fatherid
+				+ ", funses=" + funses + ", roleses=" + roleses + ", roleList="
+				+ roleList + ", isChecked=" + isChecked + ", menus=" + menuList
+				+ "]";
+	}
+
+	private String menuname;
+	private String connurl;
+	private Long fatherid;
+	private Set funses = new HashSet(0);
+	private Set roleses = new HashSet(0);
+	private List<Roles> roleList = new ArrayList<Roles>();//角色  菜单表
+	private String isChecked;
+	private List<Menus> menuList=new ArrayList<Menus>();
+
+
+
+	public String getIsChecked() {
+		return isChecked;
+	}
+
+	public List<Roles> getRoleList() {
+		return roleList;
+	}
+
+	public void setRoleList(List<Roles> roleList) {
+		this.roleList = roleList;
+	}
+
+	public List<Menus> getMenuList() {
+		return menuList;
+	}
+
+	public void setMenuList(List<Menus> menuList) {
+		this.menuList = menuList;
+	}
+
+	public void setIsChecked(String isChecked) {
+		this.isChecked = isChecked;
+	}
+
+
+	public Menus() {
+	}
+
+
+	public Menus(Long menuid, String menuname, Long fatherid) {
+		this.menuid = menuid;
+		this.menuname = menuname;
+		this.fatherid = fatherid;
+	}
+
+
+	public Menus(Long menuid, String menuname, String connurl, Long fatherid,
+				 Set funses, Set roleses) {
+		this.menuid = menuid;
+		this.menuname = menuname;
+		this.connurl = connurl;
+		this.fatherid = fatherid;
+		this.funses = funses;
+		this.roleses = roleses;
+	}
+
+
+
+	public Long getMenuid() {
+		return this.menuid;
+	}
+
+	public void setMenuid(Long menuid) {
+		this.menuid = menuid;
+	}
+
+	public String getMenuname() {
+		return this.menuname;
+	}
+
+	public void setMenuname(String menuname) {
+		this.menuname = menuname;
+	}
+
+	public String getConnurl() {
+		return this.connurl;
+	}
+
+	public void setConnurl(String connurl) {
+		this.connurl = connurl;
+	}
+
+	public Long getFatherid() {
+		return this.fatherid;
+	}
+
+	public void setFatherid(Long fatherid) {
+		this.fatherid = fatherid;
+	}
+
+	public Set getFunses() {
+		return this.funses;
+	}
+
+	public void setFunses(Set funses) {
+		this.funses = funses;
+	}
+
+	public Set getRoleses() {
+		return this.roleses;
+	}
+
+	public void setRoleses(Set roleses) {
+		this.roleses = roleses;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((menuid == null) ? 0 : menuid.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		final Menus other = (Menus) obj;
+		if (menuid == null) {
+			if (other.menuid != null)
+				return false;
+		} else if (!menuid.equals(other.menuid))
+			return false;
+		return true;
+	}
+
+}
